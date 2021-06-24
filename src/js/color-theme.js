00d09b16b44f14911a1b62a73426560c883cc396
chillhaze <65725016+chillhaze@users.noreply.github.com>
2021-06-25 01:21:12 +0300

    added theme change function

--- src/js/color-theme.js
+++ src/js/color-theme.js
@@ -1,35 +1,44 @@
+import { Theme } from './constants';
+
+const body = document.querySelector('body');
 const toggle = document.querySelector('#theme-switch-toggle');
-
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
-
 toggle.addEventListener('change', onToggleChange);
 
-function onToggleChange(e) {
-  if (toggle.checked) {
-    document.querySelector('body').classList.remove('light-theme');
-    document.querySelector('body').classList.add('dark-theme');
-    console.log(localstorage);
+// установка темы при загрузке страницы
+window.onload = () => {
+  const actualUserTheme = localStorage.getItem('theme');
+
+  if (actualUserTheme === Theme.DARK) {
+    body.classList.add(Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+    toggle.checked = localStorage.getItem('chkbx-active');
   } else {
-    document.querySelector('body').classList.remove('dark-theme');
-    document.querySelector('body').classList.add('light-theme');
+    body.classList.add(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+};
+
+// переключение темы через чекбокс
+function onToggleChange() {
+  const userTheme = localStorage.getItem('theme');
+  let chkbxValue = toggle.checked;
+  console.log(chkbxValue);
+
+  if (userTheme === Theme.LIGHT) {
+    localStorage.setItem('chkbx-active', chkbxValue);
+    localStorage.removeItem(userTheme);
+    localStorage.setItem('theme', Theme.DARK);
+
+    body.classList.remove(Theme.LIGHT);
+    body.classList.add(Theme.DARK);
+    // console.log(localStorage.getItem('theme'));
+  } else {
+    localStorage.setItem('chkbx-active', chkbxValue);
+    localStorage.removeItem(userTheme);
+    localStorage.setItem('theme', Theme.LIGHT);
+
+    body.classList.remove(Theme.DARK);
+    body.classList.add(Theme.LIGHT);
+    // console.log(localStorage.getItem('theme'));
   }
 }
-
-// let theme = 'light';
-// function onToggleChange(e) {
-//   if (theme === 'light') {
-//     theme = 'dark';
-//
-//   } else {
-//     theme === 'light';
-//     document.querySelector('body').classList.add('light-theme');
-//   }
-// }
-
-// function onToggleChange(e) {
-//   console.log(e.target);
-//
-// }
